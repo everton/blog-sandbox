4c9c782849af5cad91e4aa24e9cf0b3f960ff9dd
Everton J. Carpes <everton.carpes@gmail.com>
2012-07-27 15:30:10 -0300

    Rails init 3.2.6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogSandbox::Application.config.secret_token = '52c725247fcf9891b860d14b0a984449c3ce6d28571c69c1a1ed8a10f288263fea1d6f2f0d04ee60487f9fe8b86a00f46e0ca7cb85075d7c64eb1b8000aca3ba'
